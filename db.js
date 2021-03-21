54db1a4abf63622dd99077eaccf7bc3106f1e2f3
askhat <askat.shailoobekov@gmail.com>
2021-03-21 22:52:34 +0600

    1

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 const Pool = require('pg').Pool;
 const pool = new Pool({
-    user: 'ckjmqluvjeaunu',
-    password: '73f708681e5d1a43334511d93a621f4210ce2e21242f4a9325a483938b1d3891',
-    host: 'ec2-54-205-183-19.compute-1.amazonaws.com',
+    user: 'postgres',
+    password: 'test123',
+    host: 'localhost',
     port: 5432,
-    database: 'd7mcigtamkthj6'
+    database: 'perntodo'
 });
 
 module.exports = pool;
